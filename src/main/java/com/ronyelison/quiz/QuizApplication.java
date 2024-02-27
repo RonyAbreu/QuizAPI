2fadd5ad6d63da65a8fe66b7a303bea0f9de944b
RonyAbreu <ronyelison78@gmail.com>
2024-02-26 21:51:20 -0300

    Style: alterando banner e environment da versão

--- src/main/java/com/ronyelison/quiz/QuizApplication.java
+++ src/main/java/com/ronyelison/quiz/QuizApplication.java
@@ -1,13 +1,26 @@
 package com.ronyelison.quiz;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class QuizApplication {
+	@Value("${app.version}")
+	private String version;
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizApplication.class, args);
+	}
+
+	@GetMapping("/")
+	@ResponseBody
+	public String welcome(){
+		return "Bem vindo a QUIZ-API ".concat(version);
 	}
 
 }

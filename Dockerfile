a11cddc2405d86fd27e4e52c275574b4cf32260c
RonyAbreu <ronyelison78@gmail.com>
2024-02-28 15:53:40 -0300

    Build: reconfigurando arquivos Dockerfile e docker-compose.yaml

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM maven:3.9.6-amazoncorretto-21
+FROM amazoncorretto:21.0.2-alpine3.19
 WORKDIR /app
-COPY . .
-RUN mvn package -DskipTests
+ARG JAR_FILE=target/QuizAPI.jar
+COPY ${JAR_FILE} app.jar
 EXPOSE 8080
-CMD ["java", "-jar", "/app/target/QuizAPI.jar"]
+CMD ["java", "-jar", "app.jar"]
--- src/main/java/com/ronyelison/quiz/QuizApplication.java
+++ src/main/java/com/ronyelison/quiz/QuizApplication.java
@@ -2,13 +2,16 @@ package com.ronyelison.quiz;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @Controller
+@EnableAutoConfiguration(exclude = SecurityAutoConfiguration.class)
 public class QuizApplication {
 	@Value("${app.version}")
 	private String version;

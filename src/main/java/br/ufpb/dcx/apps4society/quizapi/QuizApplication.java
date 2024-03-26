c328f80c8a38924479c5f61629d52da014d050b9
RonyAbreu <ronyelison78@gmail.com>
2024-03-26 14:45:11 -0300

    Refactor: refatorando retorno do endpoint raiz, para retornar um HTML

--- src/main/java/br/ufpb/dcx/apps4society/quizapi/QuizApplication.java
+++ src/main/java/br/ufpb/dcx/apps4society/quizapi/QuizApplication.java
@@ -18,7 +18,25 @@ public class QuizApplication {
 
 	@GetMapping("/")
 	public String welcome(){
-		return "Bem vindo a QUIZ-API ".concat(version);
+		return welcomeHtml();
+	}
+
+	private String welcomeHtml(){
+		return "<div style=\"display: flex; justify-content: center;\">\n" +
+				"      <h1\n" +
+				"        style=\"\n" +
+				"          box-shadow: 0px 0px 8px black;\n" +
+				"          border-radius: 15px;\n" +
+				"          width: 500px;\n" +
+				"          height: 80px;\n" +
+				"          display: flex;\n" +
+				"          align-items: center;\n" +
+				"          justify-content: center;\n" +
+				"        \"\n" +
+				"      >\n" +
+				"        Bem-vindo ao QuizAPI <span style=\"color: green; margin-left: 10px;\">"+version+"</span>\n" +
+				"      </h1>\n" +
+				"    </div>";
 	}
 
 }

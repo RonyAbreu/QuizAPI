39a9c1c6c4ca116d11f7e42ec71ac051fb30e969
RonyAbreu <ronyelison78@gmail.com>
2024-04-02 15:04:03 -0300

    Feat: adicionando validação ao campo url da Questão

--- src/main/java/br/ufpb/dcx/apps4society/quizapi/dto/question/QuestionUpdate.java
+++ src/main/java/br/ufpb/dcx/apps4society/quizapi/dto/question/QuestionUpdate.java
@@ -2,10 +2,13 @@ package br.ufpb.dcx.apps4society.quizapi.dto.question;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.URL;
 
 public record QuestionUpdate(
         @NotBlank(message = "Campo de titulo não pode ser vazio")
         @Size(min = 4 ,max = 500, message = "Número de caracteres inválido")
         String title,
+        @Size(max = 255, message = "Número de caracteres inválido")
+        @URL(message = "URL inválida")
         String imageUrl) {
 }

36856ec2bba530ad13e42709637ecede3a3f58fc
RonyAbreu <ronyelison78@gmail.com>
2024-04-22 13:10:10 -0300

    Refactor: ordenando página de questões pelo id

--- src/main/java/br/ufpb/dcx/apps4society/quizapi/controller/QuestionController.java
+++ src/main/java/br/ufpb/dcx/apps4society/quizapi/controller/QuestionController.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -104,8 +105,10 @@ public class QuestionController {
                                                                          @PathVariable Long idTheme,
                                                                          @RequestParam(value = "title", defaultValue = "") String title,
                                                                          @RequestParam(value = "page", defaultValue = "0") Integer page,
-                                                                         @RequestParam(value = "size", defaultValue = "20") Integer size){
-        Pageable pageable = PageRequest.of(page,size);
+                                                                         @RequestParam(value = "size", defaultValue = "20") Integer size,
+                                                                         @RequestParam(value = "direction", defaultValue = "asc") String direction){
+        Sort.Direction pageDirection = "desc".equalsIgnoreCase(direction) ? Sort.Direction.DESC : Sort.Direction.ASC;
+        Pageable pageable = PageRequest.of(page,size, Sort.by(pageDirection,"id"));
         return ResponseEntity.ok(service.findQuestionsByCreatorAndTheme(token, title, idTheme, pageable));
     }
 

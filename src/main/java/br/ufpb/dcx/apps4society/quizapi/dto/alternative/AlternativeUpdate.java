f2f135a73bbd344e8f700972f695e308438a70a4
RonyAbreu <ronyelison78@gmail.com>
2024-04-22 12:54:01 -0300

    Refactor: alterando o número de caracteres suportados pelas questões e alternativas

--- src/main/java/br/ufpb/dcx/apps4society/quizapi/dto/alternative/AlternativeUpdate.java
+++ src/main/java/br/ufpb/dcx/apps4society/quizapi/dto/alternative/AlternativeUpdate.java
@@ -5,6 +5,6 @@ import jakarta.validation.constraints.Size;
 
 public record AlternativeUpdate(
         @NotBlank(message = "Campo de resposta não pode ser vazio")
-        @Size(min = 1, max = 255, message = "Número de caracteres inválido")
+        @Size(min = 1, max = 100, message = "Número de caracteres inválido")
         String text) {
 }

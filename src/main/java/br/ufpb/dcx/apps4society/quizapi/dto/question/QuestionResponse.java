5c496bf5a7e31e8911d340ffe774c480b7e0a9a1
RonyAbreu <ronyelison78@gmail.com>
2024-04-23 15:28:38 -0300

    Refactor: retornando o Id do criador ao buscar uma questão

--- src/main/java/br/ufpb/dcx/apps4society/quizapi/dto/question/QuestionResponse.java
+++ src/main/java/br/ufpb/dcx/apps4society/quizapi/dto/question/QuestionResponse.java
@@ -4,11 +4,13 @@ import br.ufpb.dcx.apps4society.quizapi.dto.alternative.AlternativeResponse;
 import br.ufpb.dcx.apps4society.quizapi.dto.theme.ThemeResponse;
 
 import java.util.List;
+import java.util.UUID;
 
 public record QuestionResponse(
          Long id,
          String title,
          String imageUrl,
+         UUID creatorId,
          ThemeResponse theme,
          List<AlternativeResponse> alternatives) {
 }
